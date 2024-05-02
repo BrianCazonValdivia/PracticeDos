5001745e23441fccdb64335c5dc8fc18545b60d1
BrianCazon <briancazon1@upb.edu>
2024-05-02 15:15:50 -0400

    P3-001 added patientController and endpoints using CRUD functions from BusinessLogic as Dependency

---
+++ PracticeDos/Controllers/PatientController.cs
@@ -0,0 +1,48 @@
+﻿using BusinessLogic.Manager;
+using BusinessLogic.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UPB.PracticeDos.Controllers
+{
+    [ApiController]
+    public class PatientController : ControllerBase
+    {
+        private PatientManager _patienController;
+        public PatientController()
+        {
+            _patienController = new PatientManager();
+        }
+
+        [HttpGet]
+        public List<Patient> Get()
+        {
+            return _patienController.GetAll();
+        }
+
+        [HttpGet]
+        [Route("{ci}")]
+        public Patient Get(int ci)
+        {
+            return _patienController.GetById(ci);
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Patient patient)
+        {
+            _patienController.CreatePatient(patient);
+        }
+
+        [HttpPut("{ci}")]
+        public void Put(int ci, [FromBody] Patient patient)
+        {
+            _patienController.UpdatePatient(ci, patient);
+        }
+
+        [HttpDelete("{ci}")]
+        public void Delete(int ci)
+        {
+            _patienController.DeletePatient(ci);
+        }
+    }
+}
